da18bc3e1eb1ebfd314e92e24030a475413a766b
Chao Wan <chaowan@Chaos-Mac-mini.charter.com>
2024-01-16 22:43:38 -0800

    first commit

---
+++ src/main/java/com/dreamchaser/Application.java
@@ -0,0 +1,17 @@
+package com.dreamchaser;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//当有多个数据源时就得这样设置
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@MapperScan("com.dreamchaser.mapper")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
